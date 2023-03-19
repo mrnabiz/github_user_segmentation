1e4e6e449cb37929bbfbb5e3bdcf704702b2c151
Mohammad Reza Nabizadeh <mrnabiz@student.ubc.ca>
2023-03-19 16:18:14 -0700

    added sankey

--- src/app.py
+++ src/app.py
@@ -7,13 +7,18 @@ import dash_bootstrap_components as dbc
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
-# Load the data
+# Load the data for ML
 input_file = '../data/processed/transformed_df.h5'
 key = 'data'
 transformed_df = pd.read_hdf(input_file, key)
-
-#transformed_df = pd.read_csv('data/processed/transformed_df.csv', index_col=0)
 col_list_dropdown = transformed_df.columns.to_list()
+
+# Read the data for Sankey
+combined_df = pd.read_csv('../data/processed/combined_sankey.csv.zip',
+                          index_col=0,
+                          compression='zip')
+sankey_col_list = combined_df['type'].unique().tolist()
+sankey_col_list.remove('Start')
 
 # Styles and bootstraping
 ext_styl = [
@@ -44,7 +49,7 @@ server = app.server
 # Set the dashboard title
 app.title = 'GitHub User Segmentation'
 
-# Widgets
+# Widgets for ML
 event_selector = dcc.Dropdown(
     id="event_filter",
     placeholder="Event Types",
@@ -67,16 +72,50 @@ component_selector = dcc.Dropdown(
     value=10
     )
 
+# Widgets for Sankey
+event_selector_sankey = dcc.Dropdown(
+    id="event_filter_sankey",
+    placeholder="Event Types",
+    options=[
+        {"label": i, "value": i} for i in sankey_col_list
+    ],
+    clearable=True,
+    searchable=True,
+    multi=True,
+    value=sankey_col_list
+    )
+
+step_selector = dcc.Dropdown(
+    id="n_steps",
+    placeholder="Number of Steps",
+    options=[
+        {"label": i, "value": i} for i in range(3, 11)
+    ],
+    clearable=False,
+    value=5
+    )
+
+depth_selector = dcc.Dropdown(
+    id="events_per_step",
+    placeholder="Number of Events/Step",
+    options=[
+        {"label": i, "value": i} for i in range(2, 16)
+    ],
+    clearable=False,
+    value=5
+    )
+
 
 app.layout = dbc.Container([
-    html.H1("🤖 GitHub User Segmentation", style={'textAlign': 'start'}),
+    html.H1("🤖 GitHub User Segmentation and Behavior Dashboard", style={'textAlign': 'start'}),
     html.P("A dashboard to take a deeper look into GitHub users' behavior on March 17, 2023, Why only March 17th? Because there is over 4M recorded events on only one day!😬", 
            style={'textAlign': 'start'}),
     html.Hr(),
+    html.H5("User Segmentation", style={'textAlign': 'start'}),
     dbc.Row([
         dbc.Col([html.P('Select the Events:'),
                  event_selector], width=9),
-        dbc.Col([html.P('Select the Number of Clusters:'),
+        dbc.Col([html.P('Number of Clusters:'),
                  component_selector], width=3)]),
     html.Hr(),
     dbc.Card([
@@ -87,6 +126,20 @@ app.layout = dbc.Container([
     dbc.Card([
        dbc.CardHeader("🗿 PCA Visualization"),
        dbc.CardBody([dbc.Spinner(dcc.Graph(id="pca-plot"))]),
+       ]),
+    html.Br(),
+    html.H5("User Behavior Analysis", style={'textAlign': 'start'}),
+    dbc.Row([
+        dbc.Col([html.P('Select the Events for Behavior Diagram:'),
+                 event_selector_sankey], width=8),
+        dbc.Col([html.P('Number of Steps:'),
+                 step_selector], width=2),
+        dbc.Col([html.P('Number of Events/Step:'),
+                 depth_selector], width=2)]),
+    html.Hr(),
+    dbc.Card([
+       dbc.CardHeader("🏄 Sankey User Behavior Analysis"),
+       dbc.CardBody([dbc.Spinner(dcc.Graph(id="sankey-plot"))]),
        ]),
     html.Br(),
     html.P(
@@ -100,13 +153,15 @@ app.layout = dbc.Container([
 # Set up callbacks/backend
 @app.callback(
         [Output("cluster_viz", "figure"),
-         Output("pca-plot", "figure")],
+         Output("pca-plot", "figure"),
+         Output("sankey-plot", "figure")],
         [Input('event_filter', 'value'),
-         Input('n_component', 'value')]
-)
+         Input('n_component', 'value'),
+         Input('event_filter_sankey', 'value'),
+         Input('n_steps', 'value'),
+         Input('events_per_step', 'value')])
 
-
-def update_charts(event_filter, n_component):
+def update_charts(event_filter, n_component, event_filter_sankey, n_steps, events_per_step):
 
     cols_list = transformed_df.columns.to_list()
 
@@ -153,7 +208,9 @@ def update_charts(event_filter, n_component):
     fig_2.update_layout(
         xaxis_title="Features",
         yaxis_title="Principal Components",
-        xaxis = {'side': 'top',  'tickangle':300}
+        xaxis = {'side': 'top',  'tickangle':300},
+        width=1080,
+        height=600
         )
     fig_2.update_layout(autosize=True)
 
@@ -203,13 +260,130 @@ def update_charts(event_filter, n_component):
             yaxis=dict(title=c2),
             zaxis=dict(title=c3),
         ),
-        width=900,
+        width=1080,
         height=600)
     
     fig_1 = go.Figure(data=[trace], layout=layout)
     fig_1.update_layout(autosize=True)
 
-    return fig_1, fig_2
+
+    # Sankey Plot
+    
+    # Required Function
+    def filter_starting_step(x, starting_step, n_steps):
+        starting_step_index = x.index(starting_step)
+        
+        return x[starting_step_index: starting_step_index + n_steps] 
+
+    sank_col_list = event_filter_sankey.copy()
+    sank_col_list.append('Start')
+    sankey_df = combined_df[combined_df['type'].isin(sank_col_list)]
+    starting_step = 'Start'
+    valid_ids = sankey_df[sankey_df['type'] == starting_step]['actor_id'].unique()
+    
+    n_steps = n_steps
+
+    # plan out the journey per user, with each step in a separate column
+    flow = combined_df[(combined_df['actor_id'].isin(valid_ids))]\
+        .groupby('actor_id')\
+        .type.agg(list)\
+        .to_frame()['type']\
+        .apply(lambda x: filter_starting_step(x, starting_step, n_steps))\
+        .to_frame()\
+        ['type'].apply(pd.Series)
+
+    flow = flow.fillna('End')
+
+    for i, col in enumerate (flow.columns):
+        flow[col] = 'Step {}: '.format(i+1) + flow[col].astype(str)
+        
+    events_per_step = events_per_step
+
+    for col in flow.columns:
+        all_events = flow[col].value_counts().index.tolist()
+        all_events = [e for e in all_events if e!=(str(col+1) + ': End' )]
+        top_events = all_events[:events_per_step]
+        to_replace = list(set(all_events) - set(top_events))
+        flow[col].replace(to_replace, [str(col+1) +': Other'] * len(to_replace), inplace=True)
+
+    flow = flow.groupby(list(range(n_steps)))\
+        .size()\
+        .to_frame()\
+        .rename({0: 'Count'}, axis=1)\
+        .reset_index()
+    
+    label_list = []
+    cat_cols = flow.columns[:-1].values.tolist()
+    for cat_col in cat_cols:
+        label_list_temp = list(set(flow[cat_col].values))
+        label_list = label_list + label_list_temp
+
+    colors_list = label_list.copy()
+    for i, item in enumerate(label_list):
+        if (item.__contains__('Start') | item.__contains__('End')):
+            colors_list[i] = '#8C564B'
+        elif item.__contains__('Push'):
+            colors_list[i] = '#636EFA'
+        elif item.__contains__('Create'):
+            colors_list[i] = '#EF553B'
+        elif item.__contains__('Pull Request'):
+            colors_list[i] = '#00CC96'
+        elif item.__contains__('Watch'):
+            colors_list[i] = '#FFA15A'
+        elif item.__contains__('Issue'):
+            colors_list[i] = '#19D3F3'
+        elif item.__contains__('Issue'):
+            colors_list[i] = '#FECB52'
+        elif item.__contains__('Other'):
+            colors_list[i] = '#7F7F7F'
+        else:
+            colors_list[i] = '#7F7F7F'
+
+    for i in range(len(cat_cols) - 1):
+        if i == 0:
+            source_target_df = flow[[cat_cols[i], cat_cols[i + 1], 'Count']]
+            source_target_df.columns = ['Source', 'Target', 'Count']
+        else:
+            temp_df = flow[[cat_cols[i], cat_cols[i + 1], 'Count']]
+            temp_df.columns = ['Source', 'Target', 'Count']
+            source_target_df = pd.concat([source_target_df, temp_df])
+            source_target_df = source_target_df.groupby(['Source', 'Target']).agg({'Count': 'sum'}).reset_index()
+
+    # add index for source-target pair
+    source_target_df['Source_id'] = source_target_df['Source'].apply(lambda x: label_list.index(x))
+    source_target_df['Target_id'] = source_target_df['Target'].apply(lambda x: label_list.index(x))
+
+    # filter out the end step
+    source_target_df = source_target_df[(~source_target_df['Source'].str.contains('End')) &
+                                        (~source_target_df['Target'].str.contains('End'))]
+
+    fig_3 = go.Figure(data=[go.Sankey(
+    node = dict(
+        pad=20,
+        thickness=20,
+        color=colors_list,
+        line=dict(
+            color="black",
+            width=0.5),
+        label=label_list),
+    link = dict(
+        source=source_target_df['Source_id'].values.tolist(),
+        target=source_target_df['Target_id'].values.tolist(),
+        value=source_target_df['Count'].astype(int).values.tolist(),
+        hoverlabel=dict(
+            bgcolor='#C2C4C7')
+            ))])
+
+    fig_3.update_layout(dict(
+        height=600,
+        width=1080,
+        margin=dict(t=30, l=0, r=0, b=30),
+        font=dict(size=10),
+        autosize=True
+        ))
+
+
+    return fig_1, fig_2, fig_3
 
 
 if __name__ == '__main__':
